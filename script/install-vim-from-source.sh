f151e2ea18355b28758d92e5aeb5ef927d98e709
zhitian deng <allen.zt.d@gmail.com>
2020-08-01 17:08:09 +0800

    add script to compile vim from source on ubuntu 16.04
    Signed-off-by: zhitian deng <allen.zt.d@gmail.com>

---
+++ script/install-vim-from-source.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#NOTE: test on Ubuntu 16.04
+#FEATRUE: support python3, disable GUI
+
+#install dependency
+sudo apt install libncurses5-dev python-dev python3-dev ruby-dev lua5.1 liblua5.1-0-dev libperl-dev git
+
+#remove old vim
+sudo apt remove $(dpkg -l | grep vim | awk '{print $2}' | xargs)
+
+cd ~
+git clone https://github.com/vim/vim.git vim-source
+[ "$?" != "0" ] && echo "clone failed, exit" && exit
+
+cd vim-source
+
+./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp=yes \
+            --enable-pythoninterp=yes \
+            --with-python-config-dir=$(python3-config --configdir) \
+            --enable-perlinterp=yes \
+            --enable-luainterp=yes \
+            --enable-cscope \
+            --disable-gui \
+            --without-x \
+            --prefix=/usr
+
+sudo make V=s
+sudo make install
+
+cd ~
+sudo rm -rf vim-source
