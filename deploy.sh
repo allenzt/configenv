fb554e2c595fce951cc7b95f2594b3ee42e2f5b5
allen <allen.zt.d@gmail.com>
2020-12-13 17:21:48 +0800

    add support for Ubuntu 20.04
    Signed-off-by: allen <allen.zt.d@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -65,13 +65,23 @@ tar -cvf - vim | tar -xvf - -C $HOME && mv $HOME/vim $HOME/.vim
 
 #Install ccls and Nodejs for ubuntu 16.04
 cat /etc/lsb-release  | grep -o 'Ubuntu 16.04'
-[ "$?" = "0" ] && {
-    echo "Install ccls for Ubuntu 16.04"
-    ln -sf $HOME/tools/ccls-ubuntu-16.04 ccls
+DISTRO_ID=$(cat /etc/lsb-release  | grep DISTRIB_ID | awk -F= '{print $NF}')
+DISTRO_RELEASE=$(cat /etc/lsb-release  | grep DISTRIB_RELEASE | awk -F= '{print $NF}')
 
-    curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-    sudo apt install nodejs=12.18.3-1nodesource1
-}
+case "${DISTRO_ID}-${DISTRO_RELEASE}" in
+    Ubuntu-20.04)
+        curl -sL install-node.now.sh/lts | bash
+        sudo apt install ccls
+        ;;
+    Ubuntu-16.04)
+        echo "Install ccls for Ubuntu 16.04"
+        ln -sf $HOME/tools/ccls-ubuntu-16.04 ccls
 
+        curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+        sudo apt install nodejs=12.18.3-1nodesource1
+        ;;
+esac
 
+cd ${HOME}/.config/coc/extensions/node_modules/coc-ccls && ln -s node_modules/ws/lib
+sudo npm i -g bash-language-server
 #######INSTALL END#######
