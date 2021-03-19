36f0ffe57c2e868512b53512394e218d49a0c4f3
allen <allen.zt.d@gmail.com>
2021-03-19 23:38:04 +0800

    can use c-x c-e to edit command line
    Signed-off-by: allen <allen.zt.d@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,7 @@ echo "alias rm='rm -i'" >> bashrc
 echo "alias man=\"LESS_TERMCAP_mb=$'\e[01;31m' LESS_TERMCAP_md=$'\e[01;38;5;170m' LESS_TERMCAP_me=$'\e[0m' LESS_TERMCAP_se=$'\e[0m' LESS_TERMCAP_so=$'\e[38;5;246m' LESS_TERMCAP_ue=$'\e[0m' LESS_TERMCAP_us=$'\e[04;38;5;74m' man\"" >>bashrc
 echo "source $tools_dir/aliasfile" >> bashrc
 echo "PATH=$PATH:$tools_dir" >> bashrc
+echo "export EDITOR=vim" >> bashrc
 
 # cat >> bashrc <<'EOF'
 # export LESS_TERMCAP_md=$'\E[01;31m'
