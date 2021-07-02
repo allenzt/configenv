54c18a88021ae549ed3f9fb807e0b014352bf3a0
allen <allen.zt.d@gmail.com>
2021-07-02 17:47:36 +0000

    add global install script for ubuntu 16.04
    Signed-off-by: allen <allen.zt.d@gmail.com>

---
+++ script/install-global-for-ubuntu-16.04.sh
@@ -0,0 +1,3 @@
+sudo add-apt-repository ppa:alexmurray/global
+sudo apt update
+sudo apt install global=6.5.7-1~bpo16.04+1
--- script/install-nodejs-binary-for-ubuntu-16.04.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt install nodejs=12.18.3-1nodesource1
