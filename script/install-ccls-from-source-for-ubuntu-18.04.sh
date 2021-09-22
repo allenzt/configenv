a4dcd9033860acfd157f0236d59c11539a90e312
allen deng <allen.zt.d@gmail.com>
2021-09-22 09:11:33 +0800

    improve ccls script for ubuntu 18.04
    Signed-off-by: allen deng <allen.zt.d@gmail.com>

--- script/install-ccls-from-source-for-ubuntu-18.04.sh
+++ script/install-ccls-from-source-for-ubuntu-18.04.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 #Install clang+llvm
 sudo apt update
-sudo apt-get install zlib1g zlib1g-dev
-sudo apt install clang-8 libclang-8-dev
+sudo apt-get install zlib1g zlib1g-dev libclang-8-dev
+sudo apt install clang-8
 sudo apt install cmake
 
-sudo ln -sf /usr/bin/clang-8 /usr/bin/clang
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-8 50
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-8 50
 
 #Install ccls
 git clone --depth=1 --recursive https://github.com/MaskRay/ccls
+
+cd ccls
 cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=clang++-8 -DCMAKE_PREFIX_PATH=/usr
 cd Release && make V=s
-cp Release/ccls ~/tools
+cp ccls ~/tools/ccls-ubuntu-18.04
 
 #Fix some issues
 cd ~/.config/coc/extensions/node_modules/coc-ccls && ln -s node_modules/ws/lib lib
